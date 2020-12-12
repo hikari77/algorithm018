29127dce4e4ac159cbf4c5a706ba52c3f13a45d7
bosco <bosco@boscos-MacBook-Pro.local>
2020-12-11 19:19:37 -0500

    revise 56

--- week08/56-MergeIntervals.js
+++ week08/56-MergeIntervals.js
@@ -6,13 +6,14 @@ var merge = function(intervals) {
     intervals.sort((a, b) => a[0] - b[0]);
     let i = 1;
     while (i < intervals.length) {
-        let left = intervals[i - 1][0];
-        let right = Math.max(intervals[i - 1][1], intervals[i][1]);
-        
+        // 先判断是否可以跳过， 再取左右边界会合理一点
         if (intervals[i - 1][1] < intervals[i][0]) {
             i++;
             continue;
         }
+        let left = intervals[i - 1][0];
+        let right = Math.max(intervals[i - 1][1], intervals[i][1]);
+        
         intervals[i - 1] = [left, right];
         intervals.splice(i, 1);
     }
